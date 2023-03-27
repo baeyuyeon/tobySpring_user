019b6b388b4f29c368a9e6fae9889fc54fd4b480
baeyuyeon <dbdus983@naver.com>
2023-03-27 23:44:40 +0900

    Initial commit

---
+++ src/main/java/com/springbook/user/TestMain.java
@@ -0,0 +1,29 @@
+package com.springbook.user;
+
+import com.springbook.user.dao.UserDao;
+import com.springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+public class TestMain {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+        
+        User user = new User();
+        user.setId("dbdus9831");
+        user.setName("배유연1");
+        user.setPassword("1234");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println("user2 Name= " + user2.getName());
+        System.out.println("user2 Password= " + user2.getPassword());
+
+        System.out.println(user.getId() + " 조회 성공");
+        
+    }
+    
+}
---
+++ src/test/java/com/springbook/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbook.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
