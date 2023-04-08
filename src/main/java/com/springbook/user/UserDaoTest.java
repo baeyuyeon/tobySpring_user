0d1d07cc41c5f158ae434d976ba121a4d7e35c79
baeyuyeon <dbdus983@naver.com>
2023-04-08 22:06:12 +0900

    xml 을 통한 빈 설정 및 의존성 주입

--- src/main/java/com/springbook/user/UserDaoTest.java
+++ src/main/java/com/springbook/user/UserDaoTest.java
@@ -4,6 +4,8 @@ import com.springbook.user.dao.*;
 import com.springbook.user.domain.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -13,14 +15,16 @@ public class UserDaoTest {
         UserDao dao2 = new OriginDaoFactory().userDao();
         System.out.println(dao);
         System.out.println(dao2);*/
-        ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
+        //ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext ac = new GenericXmlApplicationContext("applicationContext.xml");
+
 
         UserDao dao = ac.getBean("userDao", UserDao.class);
         UserDao dao2 = ac.getBean("userDao", UserDao.class);
         System.out.println("dao = " + dao);
         System.out.println("dao2 = " + dao2);
         User user = new User();
-        user.setId("3333");
+        user.setId("4444");
         user.setName("배유연12");
         user.setPassword("1234");
 
