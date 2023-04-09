423a9264d3d8b303ad1c14177b582c26a26fd23e
baeyuyeon <dbdus983@naver.com>
2023-04-09 14:57:07 +0900

    delete, count 검증

--- src/main/java/com/springbook/user/domain/User.java
+++ src/main/java/com/springbook/user/domain/User.java
@@ -1,9 +1,19 @@
 package com.springbook.user.domain;
 
 public class User {
+
+
     String id;
     String name;
     String password;
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public User() {
+    }
 
     public String getId() {
         return id;
--- src/test/java/com/springbook/user/UserDaoTest.java
+++ src/test/java/com/springbook/user/UserDaoTest.java
@@ -6,6 +6,7 @@ import com.springbook.user.domain.User;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -35,5 +36,27 @@ public class UserDaoTest {
         User user2 = dao.get(user.getId());
         assertThat(user.getName(), is(user2.getName()));
         assertThat(user.getPassword(), is(user2.getPassword()));
+    }
+
+    @Test
+    public void count() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("abc123", "배1", "123456");
+        User user2 = new User("abc124", "배2", "123456");
+        User user3 = new User("abc125", "배3", "123456");
+
+        userDao.deleteAll();
+        assertThat(userDao.getCount(), is(0));
+
+        userDao.add(user1);
+        assertThat(userDao.getCount(), is(1));
+
+        userDao.add(user2);
+        assertThat(userDao.getCount(), is(2));
+
+        userDao.add(user3);
+        assertThat(userDao.getCount(), is(3));
+
     }
 }
